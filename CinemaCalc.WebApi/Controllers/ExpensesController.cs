2c17dc845b562ab83e7e0c41f0fd3e3c2629344b
Syed M. Sawaid <34231494+SyedMSawaid@users.noreply.github.com>
2024-09-29 20:40:37 +0200

    feat: changed Expense controller to Expenses

---
+++ CinemaCalc.WebApi/Controllers/ExpensesController.cs
@@ -0,0 +1,62 @@
+using CinemaCalc.Application.Expenses;
+using CinemaCalc.Application.Expenses.Commands.CreateExpense;
+using CinemaCalc.Application.Expenses.Commands.DeleteExpense;
+using CinemaCalc.Application.Expenses.Commands.UpdateExpense;
+using CinemaCalc.Application.Expenses.Queries.GetAllExpenses;
+using CinemaCalc.Application.Expenses.Queries.GetExpenseById;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace CinemaCalc.WebApi.Controllers;
+
+public class ExpensesController : BaseController
+{
+    private readonly IMediator _mediator;
+
+    public ExpensesController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpPost(Name = "CreateExpense")]
+    [SwaggerResponse(StatusCodes.Status200OK, "returns created expense object", typeof(ExpenseDto))]
+    public async Task<IActionResult> Create([FromBody] NewExpenseDto expenseDto)
+    {
+        var expense = await _mediator.Send(new CreateExpenseCommand(expenseDto));
+        return Ok(expense);
+    }
+
+    [HttpGet(Name = "GetAllExpenses")]
+    [SwaggerResponse(StatusCodes.Status200OK, "returns list of expenses and their total amount",
+        typeof(GetAllExpensesResponse))]
+    public async Task<IActionResult> GetAll()
+    {
+        var expenses = await _mediator.Send(new GetAllExpensesCommand());
+        return Ok(expenses);
+    }
+
+    [HttpGet("{id:int}", Name = "GetExpenseById")]
+    [SwaggerResponse(StatusCodes.Status200OK, "returns expense object by ID", typeof(ExpenseDto))]
+    public async Task<IActionResult> GetById(int id)
+    {
+        var expense = await _mediator.Send(new GetExpenseByIdCommand(id));
+        return Ok(expense);
+    }
+
+    [HttpPut("{id:int}", Name = "UpdateExpense")]
+    [SwaggerResponse(StatusCodes.Status200OK, "returns updated expense object", typeof(ExpenseDto))]
+    public async Task<IActionResult> Update(int id, [FromBody] NewExpenseDto expenseDto)
+    {
+        var expense = await _mediator.Send(new UpdateExpenseCommand(id, expenseDto));
+        return Ok(expense);
+    }
+
+    [HttpDelete("{id:int}", Name = "DeleteExpense")]
+    [SwaggerResponse(StatusCodes.Status200OK, "returns deleted expense object", typeof(ExpenseDto))]
+    public async Task<IActionResult> Delete(int id)
+    {
+        var expense = await _mediator.Send(new DeleteExpenseCommand(id));
+        return Ok(expense);
+    }
+}
--- ClientApp/CinemaCalc/src/services/expense-service.ts
+++ ClientApp/CinemaCalc/src/services/expense-service.ts
@@ -3,10 +3,10 @@ import { Expense } from "../data/models";
 import { httpClient } from "../infra";
 
 export const ExpenseService = {
-  getAll: () => httpClient.get<GetAllExpensesResponse>("/Expense"),
-  getById: (id: number) => httpClient.get<Expense>(`/Expense/${id}`),
-  create: (data: Expense) => httpClient.post<Expense>("/Expense", data),
-  delete: (id: number) => httpClient.delete<Expense>(`/Expense/${id}`),
+  getAll: () => httpClient.get<GetAllExpensesResponse>("/Expenses"),
+  getById: (id: number) => httpClient.get<Expense>(`/Expenses/${id}`),
+  create: (data: Expense) => httpClient.post<Expense>("/Expenses", data),
+  delete: (id: number) => httpClient.delete<Expense>(`/Expenses/${id}`),
   update: (data: Expense) =>
-    httpClient.put<Expense>(`/Expense/${data.id}`, data),
+    httpClient.put<Expense>(`/Expenses/${data.id}`, data),
 };
